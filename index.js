34e7fec17c6672ac10e162b18528a6f5dbb5fec1
Anthony <anthonyvovo123@gmail.com>
2022-05-17 15:57:05 -0700

    fixing receinving input

--- index.js
+++ index.js
@@ -38,6 +38,7 @@ function addBookToLibrary(title, author, pages) {
   let newBook = new Book(title, author, pages);
 
   myLibrary.push(newBook);
+  console.log(myLibrary);
 }
 
 function displayBook(){
@@ -46,15 +47,15 @@ function displayBook(){
     bookDiv.classList.add('book');
 
     let title = document.createElement('h2');
-    title.innerText = `Title: ${book.title}`;
+    title.innerText = `${book.title}`;
     bookDiv.appendChild(title);
 
     let author = document.createElement('p');
-    author.innerText = `Author: ${book.author}`;
+    author.innerText = `by ${book.author}`;
     bookDiv.appendChild(author);
     
     let pages = document.createElement('p');
-    pages.innerText = `Pages: ${book.pages}`;
+    pages.innerText = `${book.pages} pages`;
     bookDiv.appendChild(pages);
     
     let read = document.createElement('p');
@@ -65,4 +66,34 @@ function displayBook(){
   })
 }
 
+function clearContainer(){
+  while(bookContainer.hasChildNodes()){
+    Array.from(bookContainer.childNodes).forEach(child => {
+      bookContainer.removeChild(child);
+    })
+  }
+}
+
 displayBook();
+
+const newBookBtn = document.getElementById('add-book');
+const newBookForm = document.getElementById('new-book-form');
+
+newBookBtn.addEventListener('click', event => {
+  newBookForm.classList.toggle('hidden');
+})
+
+const newTitle = document.getElementById('book-title');
+const newAuthor = document.getElementById('book-author').value;
+const newPages = document.getElementById('book-pages').value;
+const newRead = document.getElementById('book-read').value;
+
+newBookForm.addEventListener('submit', event => {
+  event.preventDefault();
+
+  addBookToLibrary(newTitle, newAuthor, newPages, newRead);
+  // console.log(myLibrary);
+  clearContainer();
+  displayBook();
+  // console.log(newRead.value);
+})
