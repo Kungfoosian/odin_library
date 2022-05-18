231e040d9c192c5e63df67d8053b9235ed5a3384
Anthony <anthonyvovo123@gmail.com>
2022-05-18 11:44:19 -0700

    finished script

--- index.js
+++ index.js
@@ -3,46 +3,57 @@ let myLibrary = [
     title: 'Four treasures of the sky',
     author: 'Jenny Zhang',
     pages: 326,
-    read: false
+    read: false,
   },
   {
     title: 'Fresh water for flowers',
     author: 'Valerie Perrin',
     pages: 326,
-    read: false
+    read: false,
   },
   {
     title: 'The Fervor: A Novel',
     author: 'Alma Katsu',
     pages: 326,
-    read: false
+    read: false,
   },
   {
     title: 'Mickey 7: a novel',
     author: 'Edward Ashton',
     pages: 326,
-    read: false
+    read: false,
   },
 ];
 
 const bookContainer = document.getElementById('book-container');
+const newBookBtn = document.getElementById('add-book');
+const newBookForm = document.getElementById('new-book-form');
 
-function Book(title, author, pages, read = false) {
+function clearContainer(){
+  while(bookContainer.hasChildNodes()){
+    Array.from(bookContainer.childNodes).forEach(child => {
+      bookContainer.removeChild(child);
+    })
+  }
+}
+
+function Book(title, author, pages, status) {
   this.title = title;
   this.author = author;
   this.pages = pages;
-  this.read = read;
+  this.status = status;
 }
 
-function addBookToLibrary(title, author, pages) {
-  let newBook = new Book(title, author, pages);
+function addBook(title, author, pages, status = false) {
+  let newBook = new Book(title, author, pages, status);
 
   myLibrary.push(newBook);
-  console.log(myLibrary);
 }
 
 function displayBook(){
-  myLibrary.forEach(book => {
+  clearContainer();
+
+  myLibrary.forEach((book, index) => {
     let bookDiv = document.createElement('div');
     bookDiv.classList.add('book');
 
@@ -58,42 +69,63 @@ function displayBook(){
     pages.innerText = `${book.pages} pages`;
     bookDiv.appendChild(pages);
     
-    let read = document.createElement('p');
-    read.innerText = book.read? 'Already read' : 'Haven\'t read yet' ;
-    bookDiv.appendChild(read);
+    let status = document.createElement('button');
+    status.classList.add('status', 'btn');
+    status.innerText = book.status? 'read' : 'not read' ;
+    status.addEventListener('click', changeStatus);
+    bookDiv.appendChild(status);
 
+    let removeBtn = document.createElement('button');
+    removeBtn.innerText = 'remove';
+    removeBtn.classList.add('remove', 'btn');
+    removeBtn.addEventListener('click', removeBook);
+    bookDiv.appendChild(removeBtn);
+    
+    bookDiv.id = index;
     bookContainer.appendChild(bookDiv);
   })
 }
 
-function clearContainer(){
-  while(bookContainer.hasChildNodes()){
-    Array.from(bookContainer.childNodes).forEach(child => {
-      bookContainer.removeChild(child);
-    })
-  }
+function removeBook(e){
+  let bookId = e.target.parentElement.id;
+
+  myLibrary.splice(bookId, 1);
+
+  displayBook();
 }
+
+function changeStatus(e){
+  let bookId = e.target.parentElement.id;
+  
+  myLibrary[bookId].status = myLibrary[bookId].status === true ? false : true;
+  
+  displayBook();
+}
+
+
+
 
 displayBook();
 
-const newBookBtn = document.getElementById('add-book');
-const newBookForm = document.getElementById('new-book-form');
+
 
 newBookBtn.addEventListener('click', event => {
   newBookForm.classList.toggle('hidden');
 })
 
-const newTitle = document.getElementById('book-title');
-const newAuthor = document.getElementById('book-author').value;
-const newPages = document.getElementById('book-pages').value;
-const newRead = document.getElementById('book-read').value;
-
 newBookForm.addEventListener('submit', event => {
   event.preventDefault();
+  
+  let newTitle = document.getElementById('book-title').value;
+  let newAuthor = document.getElementById('book-author').value;
+  let newPages = document.getElementById('book-pages').value;
+  let newStatus = document.getElementById('book-status').value === 'true' ? true : false;
+  
+  newBookForm.reset();
+  newBookForm.classList.toggle('hidden');
 
-  addBookToLibrary(newTitle, newAuthor, newPages, newRead);
-  // console.log(myLibrary);
-  clearContainer();
+  addBook(newTitle, newAuthor, newPages, newStatus);
+
   displayBook();
-  // console.log(newRead.value);
+
 })
